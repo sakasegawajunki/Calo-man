8b42d83f6fb9834f2545147b6619eb89ee50903e
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-07-12 13:35:58 +0000

    グラフ機能作成途中

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -2,6 +2,10 @@ class DataController < ApplicationController
   def index
     @cal_consumption = current_user.cal_consumptions
     @cal_ingestion = current_user.cal_ingestions
+    @cal_ingestions = current_user.cal_ingestions.where(created_at: 1.week.ago.beginning_of_day..Time.zone.now.end_of_day) #1週間のカロリー摂取量を定義
+    @cal_consumptions = current_user.cal_consumptions.where(created_at: 1.week.ago.beginning_of_day..Time.zone.now.end_of_day) #1週間のカロリー消費量を定義
+    @cal_ingestions_month = current_user.cal_ingestions.where(created_at: 1.month.ago.beginning_of_day..Time.zone.now.end_of_day) #1か月のカロリー摂取量を定義
   end
+  
 
 end
