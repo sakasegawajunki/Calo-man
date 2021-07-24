fbe15674f56e87888d6749cb2b2083d12efd8962
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-07-24 13:36:46 +0000

    Rspecテスト

--- spec/system/02_after_login_spec.rb
+++ spec/system/02_after_login_spec.rb
@@ -1,15 +1,9 @@
 require 'rails_helper'
 
-describe '[STEP2] ユーザログイン後のテスト' do
-  # let(:user) { create(:user) }
-  # byebug
-  # let!(:other_user) { create(:user) }
-  # user = build(:user)
-  # let!(:cal_ingestion) { create(:cal_ingestion, user: user) }
-  # let!(:other_cal_ingestion) { create(:cal_ingestion, user: other_user) }
-
+describe 'ユーザログイン後のテスト' do
+  let(:user) { create(:user) }
   before do
-    user = build(:user)
+    # @user = build(:user)
     visit new_user_session_path
     fill_in 'user[email]', with: user.email
     fill_in 'user[password]', with: user.password
@@ -17,50 +11,88 @@ describe '[STEP2] ユーザログイン後のテスト' do
   end
 
   describe '摂取カロリー入力画面のテスト' do
-    before do
-      visit cal_balances_path
-      click_on "摂取カロリー入力画面へ"
-    end
+    # before do
+    #   visit cal_balances_path
+    #   byebug
+    #   click_on "摂取カロリー入力画面へ"
+    # end
 
     context "保存成功のテスト" do
-      before do
+      # before do
+      #   visit new_cal_ingestion_path
+      #   subject { current_path }
+      # end
+      it '新しい摂取カロリーが正しく保存される' do
+        visit cal_balances_path
+        click_on "摂取カロリー入力画面へ"
         visit new_cal_ingestion_path
         subject { current_path }
+        byebug
+        fill_in 'cal_ingestion[breakfast_cal]', with: "1234"
+        fill_in 'cal_ingestion[lunch_cal]', with: "1234"
+        fill_in 'cal_ingestion[dinner_cal]', with: "1234"
+        fill_in 'cal_ingestion[snack_cal]', with: "1234"
+        page.execute_script("$('#cal_ingestion_date').val('2021-07-17')")
+        click_on '保存する'
+
+        is_expected.to have_content '保存成功のテスト'
+        expect(CalIngestion.last.breakfast_cal).to eq 1234
+        expect(CalIngestion.last.lunch_cal).to eq 1234
+        expect(CalIngestion.last.dinner_cal).to eq 1234
+        expect(CalIngestion.last.snack_cal).to eq 1234
+        expect(has_current_path?('/cal_balances?created_date=2021-07-17')).to be_truthy
       end
-      # byebug
-      it '新しい摂取カロリーが正しく保存される' do
-          fill_in 'cal_ingestion[breakfast_cal]', with: "1234"
-          fill_in 'cal_ingestion[lunch_cal]', with: "1234"
-          fill_in 'cal_ingestion[dinner_cal]', with: "1234"
-          fill_in 'cal_ingestion[snack_cal]', with: "1234"
-          #fill_in "cal_ingestion[date]", with: "October 26, 2021"
-          page.execute_script("$('#cal_ingestion_date').val('2021-07-17')")
+      it '新しい摂取カロリーが正しく更新される' do
+        cal_ingestion = CalIngestion.create(date: '2021-07-17', user_id: user.id)
+        visit edit_cal_ingestion_path(cal_ingestion.id)
+        subject { current_path }
+        # byebug
+        fill_in 'cal_ingestion[breakfast_cal]', with: "2345"
+        fill_in 'cal_ingestion[lunch_cal]', with: "2345"
+        fill_in 'cal_ingestion[dinner_cal]', with: "2345"
+        fill_in 'cal_ingestion[snack_cal]', with: "2345"
+        page.execute_script("$('#cal_ingestion_date').val('2021-07-17')")
+        click_on '更新する'
+      end
+    end
+    # context "更新成功のテスト" do
+    #   before do
+    #     visit cal_balances_path
+    #     visit edit_cal_ingestion_path(cal_ingestion.last.id)
+    #     byebug
+    #     subject { current_path }
+    #   end
+    #   it '新しい摂取カロリーが正しく更新される' do
+    #       fill_in 'cal_ingestion[breakfast_cal]', with: "2345"
+    #       fill_in 'cal_ingestion[lunch_cal]', with: "2345"
+    #       fill_in 'cal_ingestion[dinner_cal]', with: "2345"
+    #       fill_in 'cal_ingestion[snack_cal]', with: "2345"
+    #       page.execute_script("$('#cal_ingestion_date').val('2021-07-17')")
+    #       click_on '更新する'
+    #     end
+    #   end
+    end
+   describe '消費カロリー入力画面のテスト' do
+    before do
+      visit cal_balances_path
+      click_on "消費カロリー入力画面へ"
+    end
+    context "保存成功のテスト" do
+      before do
+        visit new_cal_consumption_path
+        subject { current_path }
+      end
+      it '新しい消費カロリーが正しく保存される' do
+          fill_in 'cal_consumption[cal_consumption]', with: "1234"
+          fill_in 'cal_consumption[action_pattern]', with: "1234"
+          page.execute_script("$('#cal_consumption_date').val('2021-07-17')")
           click_on '保存する'
 
           is_expected.to have_content '保存成功のテスト'
-          expect(CalIngestion.last.breakfast_cal).to eq 1234
+          expect(CalConsumption.last.cal_consumption).to eq 1234
+          expect(CalConsumption.last.base_cal_consumption).to eq 1234
           expect(has_current_path?('/cal_balances?created_date=2021-07-17')).to be_truthy
       end
     end
-
-      # it 'Calo-manを押すと、トップ画面に遷移する' do
-      #   # home_link = find_all('a')[1].native.inner_text
-      #   # home_link = home_link.gsub(/\n/, '').gsub/\A\s*/, '').gsub(/\s*\Z/, '')
-      #   click_link all(:xpath,'//div/a')[0]
-      #   is_expected.to eq '/'
-      # end
-      # it 'Usersを押すと、ユーザ一覧画面に遷移する' do
-      #   users_link = find_all('a')[2].native.inner_text
-      #   users_link = users_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
-      #   click_link users_link
-      #   is_expected.to eq '/users'
-      # end
-      # it 'Booksを押すと、投稿一覧画面に遷移する' do
-      #   books_link = find_all('a')[3].native.inner_text
-      #   books_link = books_link.gsub(/\n/, '').gsub(/\A\s*/, '').gsub(/\s*\Z/, '')
-      #   click_link books_link
-      #   is_expected.to eq '/books'
-      # end
-
   end
 end
