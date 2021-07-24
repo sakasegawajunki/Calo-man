e6bf689ca80d5cb49711d46cb44e6e4084ffa22b
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-07-24 06:06:29 +0000

    テスト

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.assets.paths >> Rails.root.join("app","assets","fonts")
-  config.assets.precompile >> ["*.svg", "*.eot", "*.woff", "*.ttf", "*.woff2"]
+  # config.assets.paths >> Rails.root.join("app","assets","fonts")
+  # config.assets.precompile >> ["*.svg", "*.eot", "*.woff", "*.ttf", "*.woff2"]
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
