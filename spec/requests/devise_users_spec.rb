f6f1c0f17e86edfd8d73023b80044d3473ebfb51
sakasegawajunki <j.sakasegawa18@gmail.com>
2021-07-25 08:00:24 +0000

    　home画面レイアウト修正

--- spec/requests/devise_users_spec.rb
+++ spec/requests/devise_users_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "UserAuthentications", type: :request do
+RSpec.describe "ユーザーログイン時のテスト", type: :request do
   let(:user) { create(:user) }
   let(:user_params) { attributes_for(:user) }
   let(:invalid_user_params) { attributes_for(:user, name: "") }
